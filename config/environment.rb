cf1a287d9bad0d63d664197f068183ac2281bbdb
Marcos Tapajos <tapajos@gmail.com>
2011-10-24 16:12:13 -0200

    Projeto Rails 3.1 vazio

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+ExemploPassaporteWebRails31::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExemploPassaporteWebRails31::Application.config.secret_token = 'e910937be59a6f65a5a434e193d40dc73d999e71469d04d8b89180618dd060d30fbfb17531fdd8a45ab9dde5d38d47fbe801f40caf0e5c6c8a7f09ba52ab89a1'
